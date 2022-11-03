058a94075823540e91367e587dc0b9d12234c8d9
vtsnowgirl <117380990+vtsnowgirl@users.noreply.github.com>
2022-11-03 12:47:22 -0400

    Update streamlit_app.py
    added Menu text items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ Import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boild Free-Range Egg')
+
